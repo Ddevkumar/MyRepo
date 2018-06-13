093095a4c92db929eee70a3f71a70a9d3cdceca5
Pushan <Pushan@192.168.1.4>
2018-06-13 15:28:52 +0530

    2nd commit

---
+++ FindSecHighest/src/main/java/com/nt/cj/GetSecHighest.java
@@ -0,0 +1,53 @@
+package com.nt.cj;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetSecHighest {
+	
+
+	public static void main(String[] args) {
+		/*int[] arr= {23,43,54,56,78};
+		  int highValue=arr[0];
+		  int secHighValue=arr[0];
+	
+           for(int i=0;i< arr.length;i++) {
+			if(arr[i] > highValue) {
+				highValue=secHighValue;
+				highValue=arr[i];
+			}
+			else if(arr[i] > secHighValue) {
+				secHighValue=arr[i];
+			}
+		
+	
+	}
+           System.out.println("\nSecond largest number is:" + secHighValue);*/
+	
+	
+		/*int arr[] = { 14, 46, 47, 86, 92, 52, 48, 36, 66, 85 };
+		int largest = arr[0];
+		int secondLargest = arr[0];
+		
+		System.out.println("The given array is:" );
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+"\t");
+		}
+		for (int i = 0; i < arr.length; i++) {
+ 
+			if (arr[i] > largest) {
+				 secondLargest = largest;
+				largest = arr[i];
+ 
+			} else if (arr[i] > secondLargest) {
+				secondLargest = arr[i];
+ 
+			}
+		}
+ 
+		System.out.println("\nSecond largest number is:" + secondLargest);*/
+		
+		
+	}
+
+}
---
+++ FindSecHighest/src/main/java/nit/FindSecHighest/App.java
@@ -0,0 +1,13 @@
+package nit.FindSecHighest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
